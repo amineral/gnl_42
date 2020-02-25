0824760112888fd1ae8761922af042028f55602c
Aleksandr Kuchin <mineral@MacBook-Pro-Aleksandr.local>
2020-02-25 14:01:19 +0300

    validated

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toberon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/19 16:21:19 by toberon           #+#    #+#             */
+/*   Updated: 2019/11/19 16:21:20 by toberon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 1024
+
+int		get_next_line(int fd, char **line);
+
+#endif
